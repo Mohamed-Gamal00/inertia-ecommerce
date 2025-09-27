7428c9ca40caaba36d41e63ebbecd2f5128facfa
Mohamed-Gamal00 <mohamedgamal1907a@gmail.com>
2025-09-27 17:01:33 +0300

    bootstrap config

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home'
+    // [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => app()->version(),
+    //     'phpVersion' => PHP_VERSION,
+    // ]
+);
 });
 
 require __DIR__ . '/dashboard.php';
